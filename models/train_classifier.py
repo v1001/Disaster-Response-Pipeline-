3bdef25ac5ec4f9dbe13fefcc7e0b3caa266b2ee
root <viovioviovioviovio@gmail.com>
2020-08-24 18:23:08 +0000

    Implemented training a linear SVC classifier

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,87 @@
 import sys
+# import libraries
+import re
+import pickle
+import numpy as np
+import pandas as pd
 
+# import nltk
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tag import pos_tag
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+# import classfiers
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# import feature extraction, pipeline and model selection tools
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+# import metrics
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, classification_report, roc_auc_score
+
+from sqlalchemy import create_engine
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('disaster_tweets', engine)
+    X = df['message']
+    y = df.drop(columns = ['id', 'message', 'original', 'genre'])
+    category_names = y.columns
+    
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    # remove non-letter characters
+    text = re.sub(r"[^a-zA-Z]", " ", text.lower())
+    # tokenize
+    tokens = word_tokenize(text)
+    # lemmatize
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+ 
+    return clean_tokens
+
+
+def show_results(y_test, y_pred, category_names):
+    for label in category_names:
+        true = y_test[label].values
+        pred = y_pred[label].values
+        print('Classification result for category \"{}\":'.format(label))
+        print(classification_report(true, pred))
 
 
 def build_model():
-    pass
-
+    model_SVC = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize, ngram_range = (1,2), stop_words = 'english')),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(LinearSVC(class_weight = 'balanced', random_state = 1)))
+    ])
+    return model_SVC
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = pd.DataFrame(model.predict(X_test), columns = category_names)
+    show_results(Y_test, y_pred, category_names)
 
 
 def save_model(model, model_filepath):
-    pass
+    f = open(model_filepath,'wb')
+    pickle.dump(model, f)
+    f.close()
 
 
 def main():
